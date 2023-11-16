44660462599c0539ff11548d8c870aaea96c5f27
AlgoWebBot <shimulzahan636@gmail.com>
2023-11-17 05:09:10 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello Server');
+})
+app.listen(port, () => {
+    console.log(`Server Runnig at port: ${port}`)
+})
